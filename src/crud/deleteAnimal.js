f213f7b4497766957f2ac960464412164c9955f7
xzunun <jmg201098@gmail.com>
2023-01-13 14:55:15 -0600

    Edit and Delete buttons with functionality

--- src/crud/deleteAnimal.js
+++ src/crud/deleteAnimal.js
@@ -1,3 +1,11 @@
-function deleteAnimal({ id }) {
-    console.log(id)
+import { readAnimals } from "./readAnimals"
+
+export const deleteAnimal = ({ animals, id }) => {
+    if( id === 0) {
+        animals.splice(0, 1)
+    } else {
+        animals.splice(id, id)
+    }
+    readAnimals({ animals })
+    console.log(animals)
 }
--- src/crud/readAnimals.js
+++ src/crud/readAnimals.js
@@ -1,3 +1,4 @@
+import { deleteAnimal } from './deleteAnimal'
 const parser = new DOMParser()
 const animalContainer = document.querySelector('#animal_container')
 export const readAnimals = ({ animals }) => {
@@ -12,15 +13,18 @@ export const readAnimals = ({ animals }) => {
                     <div class="d-flex align-items-center justify-content-between">
                         <small class="text-muted">12/01/2022</small>
                         <div>
-                            <span class="btn btn-outline-warning" title="Edit"><i class="fa-solid fa-pen-to-square"></i></span>
-                            <span class="btn btn-outline-danger" title="Delete"><i class="fa-solid fa-trash"></i></span>
+                            <span class="btn btn-outline-warning" id="edit_${ index }" title="Edit"><i class="fa-solid fa-pen-to-square"></i></span>
+                            <span class="btn btn-outline-danger" id="delete_${ index }" title="Delete"><i class="fa-solid fa-trash"></i></span>
                         </div>
                     </div>
                     </div>
                 </div>
             </div>
         `, 'text/html')
-        animalCard.body.childNodes.forEach((item) => console.log(item.childNodes))
         animalContainer.appendChild(animalCard.body.firstChild)
+        let buttonEdit = document.querySelector(`#edit_${ index }`)
+        let buttonDelete = document.querySelector(`#delete_${ index }`)
+        buttonEdit.addEventListener('click', () =>  console.log('edit'))
+        buttonDelete.addEventListener('click', () => deleteAnimal({ animals, id: index }))
     })
 }
